18fee8f484cb21452062d0b0dcfdb8bd7b442ec1
SNK128 <sagniknayak129@gmail.com>
2024-05-14 06:16:47 +0530

    buildFromLevelOrder

--- Trees/BinaryTrees.cpp
+++ Trees/BinaryTrees.cpp
@@ -121,6 +121,42 @@ void postorder(node* root)
     postorder(root->right);
     cout<<root->data<<" ";
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void buildFromLevelOrder(node* &root){
+
+    queue<node*> q;
+    cout<<"Enter data for root"<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+    q.push(root);
+
+    while(!q.empty()){
+        node* temp=q.front();
+        q.pop();
+
+        cout<<"Enter left node for: "<<temp->data<<endl;
+        int leftData;
+        cin>>leftData;
+
+        if(leftData!=-1){
+            temp->left=new node(leftData);
+            q.push(temp->left);
+        }
+
+
+        cout<<"Enter right node for: "<<temp ->data<<endl;
+        int rightData;
+        cin>>rightData;
+
+        if(rightData!=-1){
+            temp->right=new node(rightData);
+            q.push(temp->right);
+        }
+    }
+
+}
 
 int main(){
 
@@ -142,6 +178,8 @@ int main(){
     cout<<"Printing in postorder:";
     postorder(root);
     cout<<endl;
+
+    buildFromLevelOrder(root);
 
 
     return 0;
